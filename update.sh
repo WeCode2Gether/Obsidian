ea2170d28e13a653557208f9520d4a2e7d749c47
wecode2gether <wecode2gether@gmail.com>
2025-01-25 10:36:06 +0300

    updated

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-timestamp=$(date '+%A, %B %d, %Y %I:%M %p')
+timestamp=$(date '%B %d, %Y %I:%M %p')
 
-cp -r ~/.config/hypr ~/Desktop/WeCode2Gether/ConfigFiles/.config
-cp -r ~/.config/kitty ~/Desktop/WeCode2Gether/ConfigFiles/.config
-cp -r ~/.config/neofetch ~/Desktop/WeCode2Gether/ConfigFiles/.config
-cp -r ~/.config/zshrc ~/Desktop/WeCode2Gether/ConfigFiles/.config
-cp -r ~/.config/ytfzf ~/Desktop/WeCode2Gether/ConfigFiles/.config
-cp -r ~/.config/Vencord ~/Desktop/WeCode2Gether/ConfigFiles/.config
-cp -r ~/.config/ml4w ~/Desktop/WeCode2Gether/ConfigFiles/.config
+cp -r ~/.config/hypr ~/Desktop/WeCode2Gether/ConfigFiles/hypr
+cp -r ~/.config/kitty ~/Desktop/WeCode2Gether/ConfigFiles/kitty
+cp -r ~/.config/neofetch ~/Desktop/WeCode2Gether/ConfigFiles/neofetch
+cp -r ~/.config/zshrc ~/Desktop/WeCode2Gether/ConfigFiles/zshrc
+cp -r ~/.config/ytfzf ~/Desktop/WeCode2Gether/ConfigFiles/ytfzf
+cp -r ~/.config/Vencord ~/Desktop/WeCode2Gether/ConfigFiles/Vencord
+cp -r ~/.config/ml4w ~/Desktop/WeCode2Gether/ConfigFiles/ml4w
 cp -r ~/Documents/Obsidian\ Vault/ ~/Desktop/WeCode2Gether/Obsidion 
 cd ~/Desktop/WeCode2Gether/ConfigFiles && git add . && git commit -m "updated $timestamp" && git push
 cd ~/Desktop/WeCode2Gether/Obsidion && git add . && git commit -m "updated $timestamp" && git push
